0d02cc10de9bc3ebc149d6b9aa3d26ec0a074147
Katriel Tsepelevish <katriel.tsepelevish@gmail.com>
2024-07-25 18:43:27 +0300

    feat: implemented login and register pages

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Route } from '@angular/router';
 import { PlaygroundComponent } from './features/playground/playground.component';
+import { LoginComponent } from './core/auth/pages/login/login.component';
+import { RegisterComponent } from './core/auth/pages/register/register.component';
 
 export const appRoutes: Route[] = [
   {
     path: '',
     component: PlaygroundComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
 ];
---
+++ apps/frontend/src/app/core/auth/pages/register/register.component.ts
@@ -0,0 +1,36 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { provideIcons } from '@ng-icons/core';
+import { lucideGithub, lucideLoader } from '@ng-icons/lucide';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { HlmSpinnerComponent } from '@spartan-ng/ui-spinner-helm';
+import { HlmCardDirective } from '@spartan-ng/ui-card-helm';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [
+    RouterLink,
+    HlmButtonDirective,
+    HlmIconComponent,
+    HlmInputDirective,
+    HlmSpinnerComponent,
+    FormsModule,
+    HlmLabelDirective,
+    HlmCardDirective,
+  ],
+  providers: [provideIcons({ lucideGithub, lucideLoader })],
+  templateUrl: './register.component.html',
+})
+export class RegisterComponent {
+  isLoading = signal(false);
+
+  send() {
+    this.isLoading.set(true);
+    setTimeout(() => this.isLoading.set(false), 3000);
+  }
+}
